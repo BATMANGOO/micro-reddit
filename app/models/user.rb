b59c694398aaf88a93c0aa6947e5f696bc1b6403
Prabhjot <pjsingh1359@gmail.com>
2021-08-12 08:20:17 -0700

    add post and user reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_135602) do
+ActiveRecord::Schema.define(version: 2021_08_12_144555) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_08_12_135602) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
